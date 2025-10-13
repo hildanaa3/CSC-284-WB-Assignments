9b6fe2ab61013552e74b9257e251bbed1f8250e5
hildanaa3 <146589556+hildanaa3@users.noreply.github.com>
2025-10-12 20:14:00 -0400

    Truck.cpp

--- Unit_05_01_VehicleManagement/Truck.cpp
+++ Unit_05_01_VehicleManagement/Truck.cpp
@@ -5,31 +5,25 @@
 using std::cout;
 using std::endl;
 
-Truck::Truck() : Vehicle(), loadCapacityTons(1.0)
-{
-    cout << "[Truck ctor]\n";
+Truck::Truck() : Vehicle(), loadCapacityTons(1.0) {
+    // (optional) cout << "[Truck ctor]\n";
 }
 
 Truck::Truck(const std::string &mk, const std::string &mdl, int yr, double mil, double capacityTons)
-    : Vehicle(mk, mdl, yr, mil), loadCapacityTons(capacityTons)
-{
+    : Vehicle(mk, mdl, yr, mil), loadCapacityTons(capacityTons) {
     if (loadCapacityTons < 0.0)
         throw std::invalid_argument("Load capacity cannot be negative");
-  
 }
 
-Truck::~Truck()
+Truck::~Truck() {}
 
-
-void Truck::setLoadCapacity(double capacityTons)
-{
+void Truck::setLoadCapacity(double capacityTons) {
     if (capacityTons < 0.0)
         throw std::invalid_argument("Load capacity cannot be negative");
     loadCapacityTons = capacityTons;
 }
 
-double Truck::getFuelEfficiency() const
-{
+double Truck::getFuelEfficiency() const {
     double base = 9.0;
     if (loadCapacityTons > 5.0)
         base -= 0.1 * (loadCapacityTons - 5.0);
@@ -45,4 +39,3 @@ void Truck::displayInfo() const {
     std::cout << "Fuel Efficiency: " << std::fixed << std::setprecision(2)
               << getFuelEfficiency() << " km/l\n\n";
 }
-
