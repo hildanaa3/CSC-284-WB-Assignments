a231d0b21c05d4a9311573906e97d894e772aedc
hildanaa3 <146589556+hildanaa3@users.noreply.github.com>
2025-10-12 20:12:50 -0400

    Motorcycle.cpp

--- Unit_05_01_VehicleManagement/Motorcycle.cpp
+++ Unit_05_01_VehicleManagement/Motorcycle.cpp
@@ -4,26 +4,27 @@
 using std::cout;
 using std::endl;
 
-Motorcycle::Motorcycle() : Vehicle(), hasSidecar(false) {
-    cout << "[Motorcycle ctor]\n";
+Motorcycle::Motorcycle() : Vehicle(), hasSidecar(false) {}
+
+Motorcycle::Motorcycle(const std::string &mk, const std::string &mdl, int yr, double mil, bool sidecar)
+    : Vehicle(mk, mdl, yr, mil), hasSidecar(sidecar) {}
+
+Motorcycle::~Motorcycle() {}
+
+void Motorcycle::setHasSidecar(bool sidecar)
+{
+    hasSidecar = sidecar;
 }
 
-Motorcycle::Motorcycle(const std::string& mk, const std::string& mdl, int yr, double mil, bool sidecar)
-    : Vehicle(mk, mdl, yr, mil), hasSidecar(sidecar) {
-   
-}
-
-Motorcycle::~Motorcycle() 
-
-void Motorcycle::setHasSidecar(bool sidecar) { hasSidecar = sidecar; }
-
-double Motorcycle::getFuelEfficiency() const {
+double Motorcycle::getFuelEfficiency() const
+{
     return hasSidecar ? 26.0 : 30.0;
 }
 
-void Motorcycle::displayInfo() const {
+void Motorcycle::displayInfo() const
+{
     Vehicle::displayInfo();
-    std::cout << "Type: Motorcycle | Sidecar: " << (hasSidecar ? "Yes" : "No") << "\n";
-    std::cout << "Fuel Efficiency: " << std::fixed << std::setprecision(2)
-              << getFuelEfficiency() << " km/l\n\n";
+    cout << "Type: Motorcycle | Sidecar: " << (hasSidecar ? "Yes" : "No") << "\n";
+    cout << "Fuel Efficiency: " << std::fixed << std::setprecision(2)
+         << getFuelEfficiency() << " km/l\n\n";
 }
